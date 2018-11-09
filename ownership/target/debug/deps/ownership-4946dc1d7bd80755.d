b6a4d150f3294be6476a3627d9725d2f9aef78b2
Adam Tuck <tuckaet@gmail.com>
2018-11-09 01:20:52 -0800

    exer2 done

--- ownership/target/debug/deps/ownership-4946dc1d7bd80755.d
+++
@@ -1,5 +0,0 @@
-C:\Users\tucka\Desktop\rust\ownership\target\debug\deps\ownership-4946dc1d7bd80755.exe: src\main.rs
-
-C:\Users\tucka\Desktop\rust\ownership\target\debug\deps\ownership-4946dc1d7bd80755.d: src\main.rs
-
-src\main.rs:
--- ownership/target/debug/ownership.d
+++
@@ -1,1 +0,0 @@
-C:\Users\tucka\Desktop\rust\ownership\target\debug\ownership.pdb: C:\Users\tucka\Desktop\rust\ownership\src\main.rs

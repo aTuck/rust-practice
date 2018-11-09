5355689bc526dc2866a0c696daea8d1ba7f79f7e
Adam Tuck <tuckaet@gmail.com>
2018-11-08 22:56:55 -0800

    initial commit

---
+++ exercise2/src/main.rs
@@ -0,0 +1,21 @@
+fn divisors(n: i32) -> i32 {
+    return 1;
+}
+
+fn primes(n: i32) -> i32 {
+    return 1;
+}
+
+fn pythagorean(n: i32) -> i32 {
+    return 1;
+}
+
+fn main() {
+    println!("divisors(30): {}", divisors(30));
+    println!("divisors(63): {}", divisors(63));
+    println!("divisors(127): {}", divisors(127));
+    println!("primes(7): {}", primes(7));
+    println!("primes(100): {}", primes(100));
+    println!("pythagorean(10): {}", pythagorean(10));
+    println!("pythagorean(30): {}", pythagorean(30));
+}
